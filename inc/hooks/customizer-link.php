2e79d627022c0a5decc0234294178e547d78a0b7
Nikesh Ulak <cloudnikesh@gmail.com>
2018-07-19 14:51:20 +0545

    Customizer link markup changes.

--- inc/hooks/customizer-link.php
+++ inc/hooks/customizer-link.php
@@ -18,14 +18,17 @@ if ( ! function_exists('customizer_link') ) :
     if ( current_user_can('manage_options') && ( 'posts' != get_option('show_on_front')  &&  0 == $business_click_customizer_all_values['business-click-enbale-slider'] &&   0 == $business_click_customizer_all_values['business-click-enable-about-us'] &&  0 == $business_click_customizer_all_values['business-click-blog-section-enable']  &&   0 == $business_click_customizer_all_values['business-click-enable-call-to-action'] &&  0 == $business_click_customizer_all_values['business-click-contact-section-enable'] &&  0 == $business_click_customizer_all_values['business-click-feature-enable']  &&  0 == $business_click_customizer_all_values['business-click-testimonila-enable'] ) ) :
     
     ?>
-    
+
     <section class="container business-click-no-opton">
         <div class="alert alert-warning alert-dismissible fade show" role="alert">
             
+            <p>
             <?php
-                   
-                printf(esc_html__('All Section are based on page/post. Enable each Section from customizer for %1$s slider: Home page->slider->Enable feature slider. likewise to other sections %2$s %3$s click here %4$s ','business-craft'), '<br />','<br />','<a class="btn" href="' . esc_url( admin_url( 'customize.php' ) ) . '">', '</a>');
-                    ?>
+            printf(esc_html__('Enable each Section from customizer. %1$s For 
+              Feature Slider: Homepage / Front Page -> Feature Slider Section -> Show Slider. %2$s For other sections: Homepage / Front Page','business-craft'), '<br />','<br />');
+            ?>
+            </p>
+            <a class="btn" href="<?php echo esc_url( admin_url( 'customize.php' ) );?>">Customize Now</a>
 
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
