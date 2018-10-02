2ebc0cf6ca6787b33ef09321bc0c275693b97bb5
Nikesh Ulak <cloudnikesh@gmail.com>
2018-10-02 18:07:20 +0545

    Default slider img changed.

--- inc/hooks/homepage-slider.php
+++ inc/hooks/homepage-slider.php
@@ -14,7 +14,7 @@ if ( !function_exists('business_click_feature_slider_array') ) :
       global $business_click_customizer_all_values;
       $slider_excerpt_length      = absint($business_click_customizer_all_values['business-click-excerpt-length']);
 
-      $feature_slideer_array[0]['business-click-feature-title']     = esc_html__('Welcome To Business Click Pro!','business-click');
+      $feature_slideer_array[0]['business-click-feature-title']     = esc_html__('Welcome To Business Click','business-click');
       $feature_slideer_array[0]['business-click-feature-content']   =  esc_html__('Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.','business-click');
       $feature_slideer_array[0]['business-click-feature-image']     = get_template_directory_uri()."/assets/img/slider.jpg";
       $feature_slideer_array[0]['business-click-feature-url']       = '';
